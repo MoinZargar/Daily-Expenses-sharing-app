92eef6f314e670e29b63018128cb20edcf478080
MoinZargar <moinbashir2019@gmail.com>
2024-07-27 23:27:01 +0530

    user model and register controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,53 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.models.js";
+import { apiError } from "../utils/apiError.js";
 
-const registerUser = asyncHandler (async (req,res)=>{
-     res.status(200).json({
-        message : "ok"
-     })
+const registerUser = asyncHandler(async (req, res) => {
+   //take data from frontend
+   const { name, email, mobileNumber, password } = req.body;
+
+   if (
+      [name, email, mobileNumber, password].some((feild) => (
+         feild?.trim === ""
+      ))
+
+   ) {
+      throw new apiError(400, "All feilds are required")
+   }
+   //check if user already exits in database 
+   const existedUser = User.findOne({
+      $or: [
+         { name },
+         { email },
+      ]
+   })
+   if (existedUser) {
+      throw new apiError(409, "User with email or name already exists")
+   }
+
+   //store user in database
+
+   const user = await User.create({
+      name,
+      email,
+      mobileNumber,
+      password
+   })
+   // check if user is stored successfully in db and remove password and refresh Token
+   const createdUser = User.findById(user._id).select(
+      "-password -refreshToken"
+   )
+
+   if (!createdUser) {
+      throw new apiError(500, "Something went wrong while registering a user")
+   }
+
+   //return response to frontend
+
+   return res.status(201).json(
+      new apiResponse(200,createdUser,"User registered successfully")
+   )
 }
 )
 
-export {registerUser}
+export { registerUser }
---
+++ src/models/user.models.js
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import validator from 'validator';
+
+const mobileNumberRegex = /^\+?[1-9]\d{1,14}$/ 
+
+const validateMobileNumber = (mobileNumber) => {
+    return mobileNumberRegex.test(mobileNumber);
+};
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true,"Name is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: [true,"Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            validate: [validator.isEmail, 'Invalid email address'],
+        },
+        mobileNumber: {
+            type: String,
+            unique: [true, "Mobile number is already in use."],
+            validate: {
+                validator: validateMobileNumber,
+                message: "Invalid Mobile number.",
+            },
+            required:[true,"Mobile Number is required"],
+            default: "",
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken : {
+            type : String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+// hashing password before saving it in a database  
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//Password Authentication
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+//Generating Access Token
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+          _id: this._id,
+          name: this.name,
+          email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+//Generating Refresh Token
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+          _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
